8418c6d5b4a671ca9ab9eb5460d1dc4f650dfe80
Valentin Gerlach <kontakt@valentingerlach.de>
2015-05-19 22:15:31 +0200

    Projekt erstellt

---
+++ SoundCloud.NET/SoundCloudManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundCloud.NET
+{
+    /// <summary>
+    /// Stellt eine Schnittstelle zur SoundCloud API bereit
+    /// </summary>
+    public class SoundCloudManager
+    {
+
+    }
+}
