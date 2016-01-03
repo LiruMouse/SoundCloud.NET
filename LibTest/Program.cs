7a12a3316ff567c731dc44648b89aac7d31a39ac
Lirusaito <inusaito@gmail.com>
2016-01-02 19:27:46 -0500

    More pointless usings cleaned up.

--- LibTest/Program.cs
+++ LibTest/Program.cs
@@ -1,10 +1,6 @@
 ﻿using SoundCloud.NET;
 using SoundCloud.NET.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LibTest
 {
